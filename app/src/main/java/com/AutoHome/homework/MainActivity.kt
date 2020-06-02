1761a217c07fd4b5103c3aa305458f85baaaf50c
kocenen <sokodomo@kakao.com>
2020-06-02 22:03:04 +0900

    Firebase에 메시지 보내기

--- app/src/main/java/com/AutoHome/homework/MainActivity.kt
+++ app/src/main/java/com/AutoHome/homework/MainActivity.kt
@@ -6,11 +6,16 @@ import android.nfc.tech.TagTechnology
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.R.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,9 +36,33 @@ class MainActivity : AppCompatActivity() {
                 }
                 val token :String? = task.result?.token
                     Log.d("TAG",token)
-                    txt_token.setText(token.toString())
             }
 
+        val btn_send = findViewById<Button>(R.id.btn_senddata) as Button
+            btn_send.setOnClickListener {
+                BasicReadWrite(tb_sendval.text.toString())
+            }
+
+    }
+
+    fun BasicReadWrite(SendData : String)
+    {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue(SendData)
+
+        myRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d("TAG","VALUE is $value")
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("TAG","Failed to read value cause: ${databaseError.toException()}")
+            }
+        })
     }
 
 
